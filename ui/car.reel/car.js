3f9176bed0dec66fded59c82e8dfee40b664b96f
Benoit Marchant <benoit@montagestudio.com>
2015-08-01 17:52:02 +0000

    Add component Car

---
+++ ui/car.reel/car.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/car.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Car
+ * @extends Component
+ */
+exports.Car = Component.specialize(/** @lends Car# */ {
+    constructor: {
+        value: function Car() {
+            this.super();
+        }
+    }
+});
